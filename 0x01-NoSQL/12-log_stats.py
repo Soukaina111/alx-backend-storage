88a8db83077f2b9ab947d121f44e641ff6c16ad7
Soukaina111 <basmahajar@gmail.com>
2024-07-15 17:04:59 +0000

    log states

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-""" Script that provides some stats stored in MongoDB"""
+''' Python script that provides some stats about Nginx logs stored in MongoDB:
+'''
 from pymongo import MongoClient
 
 
 def print_nginx_request_logs(nginx_collection):
-    '''This function prints statistics about the Nginx request logs
+    '''Displaysstats about Nginx request logs in MongoDB.
     '''
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        count_req = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, count_req))
-    checks = len(list(
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
-    print('{} status check'.format(checks))
+    print('{} status check'.format(status_checks_count))
+
