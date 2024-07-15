00cdbcc994750fdd3a79e83f2d73720958d01975
Soukaina111 <basmahajar@gmail.com>
2024-07-15 16:56:46 +0000

    log states

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,15 +3,14 @@
 
 
 def print_nginx_request_logs(nginx_collection):
-    '''Displays stats about Nginx request logs stored in MongoDB.
-    '''
+    # This function prints statistics about the Nginx request logs.
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        num_req = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, num_req))
-    checks = len(list(
+        count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, count))
+    schecks_count = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
-    print('{} status check'.format(checks))
+    print('{} status check'.format(schecks_count))
